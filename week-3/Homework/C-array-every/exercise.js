11d2c7082942a45245d49c4865e065e307424f3e
vgl-bwk <83692259+vgl-bwk@users.noreply.github.com>
2022-03-13 20:06:18 +0100

    Finished Inclass exercises and homework A - C

--- week-3/Homework/C-array-every/exercise.js
+++ week-3/Homework/C-array-every/exercise.js
@@ -5,7 +5,11 @@
 var students = ["Omar", "Austine", "Dany", "Swathi", "Lesley", "Rukmini"];
 var group = ["Austine", "Dany", "Swathi", "Daniel"];
 
-var groupIsOnlyStudents; // complete this statement
+function isAStudent (name){
+  return students.includes(name);
+}
+
+var groupIsOnlyStudents = group.every(isAStudent)
 
 if (groupIsOnlyStudents) {
   console.log("The group contains only students");
